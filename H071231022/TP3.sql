16168cd7f6f31db83c27ce20a69d52b696d95e04
DianAlifka <145089148+DianAlifka@users.noreply.github.com>
2024-10-04 08:04:01 +0800

    kumpul tugas 1,2,3

---
+++ H071231022/TP3.sql
@@ -0,0 +1,59 @@
+SELECT * FROM books;
+INSERT  INTO authors (NAME, nationality)  -- TABEL 1
+VALUES ("tere liye", "indonesia"),
+       ("J.K. Rowling", "British"),
+       ("Andrea Hirata", '');
+
+
+SELECT id, name FROM authors;       
+INSERT INTO books (ISBN, title, author_id,  published_year, copies_available, genre) 
+VALUES ('7040289780375', 'Ayah', 3, 2015, 15, 'Fiction'), 
+       ('9780375704025', 'Bumi',1, 2014,5, 'Fantasy'), 
+       ('8310371703024', 'Bulan', 1, 2015 , 3, 'Fantasy' ), 
+       ('9780747532699', "Harry Potter and the Philosopher's Stone", 2, 1997, 10, ''),
+       ('7210301703022', 'The Running Grave', 2, 2016, 11, 'Finction');
+       
+INSERT INTO members (first_name, last_name, email, phone_number, join_date, membership_type)
+	VALUES("John","Doe","John.doe@example.com",NULL,'2023-04-29'," "),
+			("Alice","Johnson","Alice.johnson@example.com",1231231231,'2023-05-01',"Standar"),
+			("Bob","Williams","bob.williams@example.com",3213214321,'2023-06-20',"Premium");
+
+SELECT id,first_name, last_name FROM members;
+SELECT id, title FROM books;
+INSERT INTO borrowings (member_id,book_id,borrow_date,return_date)
+	VALUES(1,4,'2023-07-10','2023-07-25'),
+			(3,1,'2023-08-01',NULL),
+			(2,5,'2023-09-06','2023-09-09'),
+			(2,3,'2023-09-08',NULL),
+			(3,2,'2023-09-10',NULL);
+			
+UPDATE books
+SET copies_available = 14
+WHERE title = "ayah" ;
+
+UPDATE books
+SET copies_available = 4
+WHERE title = "bumi" ;
+
+UPDATE books
+SET copies_available = 2
+WHERE title = "bulan" ;
+
+-- alternatif
+UPDATE books
+SET copies_available = copies_available - 1
+WHERE title IN ('Ayah', 'Bulan', 'Bumi');
+
+
+UPDATE members 
+SET membership_type = "standar"
+WHERE id = 3 ;
+
+DELETE from members
+WHERE id = 2 ;
+
+ALTER TABLE borrowings				
+DROP CONSTRAINT borrowings_ibfk_1;
+
+ALTER TABLE borrowings
+ADD FOREIGN KEY(member_id) REFERENCES members(id) ON DELETE CASCADE;
